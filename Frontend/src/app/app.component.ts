e8ec09ad6b7831fa62e6411b87035d676469eeef
Ralph-Mohdad <ralph.modad06@gmail.com>
2024-01-26 17:56:56 +0100

    forms toolbar

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -12,5 +12,37 @@ export class AppComponent {
   opened = false;
 
   constructor  (private authService: AuthService, private router:Router) {}
+  // pour le formulaire
+  user=""
+  password=""
+  errorMessage: string = '';
+
+  onSubmit(event:any) {
+    if(!this.user || !this.password) {
+      this.errorMessage = 'Please fill in all fields.';
+      this.user = '';
+      this.password = '';
+      setTimeout(() => {
+        this.errorMessage = '';
+      }, 3000);
+      return;
+    }
+
+    const user = this.authService.logIn({name: this.user, password: this.password});
+    user.subscribe((user) => {
+      if(user.auth) {
+        this.authService.setToken(user.token);
+        this.router.navigate(['/home']);
+      } else {
+        this.errorMessage = 'Incorrect login or password. Please try again.';
+        console.error('Incorrect login or password');
+        this.user = '';
+        this.password = '';
+        setTimeout(() => {
+          this.errorMessage = '';
+        }, 3000);
+      }
+    });
+  }
 
 }
