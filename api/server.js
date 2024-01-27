67eb293011894eecadf5aab00f2e3307d633d451
Ilan Wanounou <wanounouilan@gmail.com>
2024-01-27 18:28:58 +0100

    remove ip

--- api/server.js
+++ api/server.js
@@ -81,8 +81,10 @@ assignment.initDb();
 
 
 // On démarre le serveur
-app.listen(port, "0.0.0.0");
-console.log('Serveur démarré sur http://localhost:' + port);
+app.listen(port, () => {
+  console.log("Serveur démarré sur http://localhost:" + port);
+});
+
 
 module.exports = app;
 
