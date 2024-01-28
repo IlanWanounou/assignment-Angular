f9babb0c48e6cd98c100135e140ef07624ac0d99
Ilan Wanounou <wanounouilan@gmail.com>
2024-01-28 03:06:59 +0100

    adding snackbar when modifying an assignment

--- Frontend/src/app/edit-assignment/edit-assignment.component.ts
+++ Frontend/src/app/edit-assignment/edit-assignment.component.ts
@@ -3,33 +3,34 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AssignmentsService } from 'src/app/shared/assignments.service';
 import { Assignment } from '../assignments/assignment.model';
 import { AuthService } from '../shared/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-edit-assignment',
   templateUrl: './edit-assignment.component.html',
-  styleUrls: ['./edit-assignment.component.css']
+  styleUrls: ['./edit-assignment.component.css'],
 })
 export class EditAssignmentComponent implements OnInit {
   assignment!: Assignment | undefined;
   nomDevoir!: string;
-  dateDeRendu!: Date; 
+  dateDeRendu!: Date;
 
   constructor(
     private assignmentsService: AssignmentsService,
     private route: ActivatedRoute,
     private router: Router,
+    private snackBar: MatSnackBar
   ) {}
- 
+
   ngOnInit(): void {
     this.getAssignment();
-   
   }
 
   getAssignment() {
     // on récupère l'id dans le snapshot passé par le routeur
     // le "+" force l'id de type string en "number"
     const id = +this.route.snapshot.params['id'];
-   
+
     this.assignmentsService.getAssignment(id).subscribe((assignment) => {
       if (!assignment) return;
       this.assignment = assignment;
@@ -40,7 +41,7 @@ export class EditAssignmentComponent implements OnInit {
   }
   onSaveAssignment() {
     if (!this.assignment) return;
- 
+
     // on récupère les valeurs dans le formulaire
     this.assignment.nom = this.nomDevoir;
     this.assignment.dateDeRendu = this.dateDeRendu;
@@ -48,11 +49,11 @@ export class EditAssignmentComponent implements OnInit {
       .updateAssignment(this.assignment)
       .subscribe((message) => {
         console.log(message);
- 
+        this.snackBar.open('Devoir modifié', 'Fermer', {
+          duration: 2000,
+        });
         // navigation vers la home page
         this.router.navigate(['/home']);
       });
   }
-
-
 }
